17855a0de93548c7a96180bebabb6856d9ccf991
EvertonMDS <emds404@gmail.com>
2022-05-18 17:05:23 -0300

    first_commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { LojasComponent } from './pages/lojas/lojas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component:HomeComponent},
+{path:'sobre', component:SobreComponent},
+{path:'contato', component:ContatoComponent},
+{path:'jobs', component:JobsComponent},
+{path:'lojas', component:LojasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { LojasComponent } from './pages/lojas/lojas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,   
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent,
+    JobsComponent,
+    LojasComponent
   ],
   imports: [
     BrowserModule,
